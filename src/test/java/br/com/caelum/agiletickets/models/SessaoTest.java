3ccdcf86d09171cf6fd5b330d16d43fc0d2bfe75
Roberto <rofregui@yahoo.com.br>
2011-12-06 11:34:54 -0200

    Sessao permite reservar todos seus ingressos disponiveis

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -30,4 +30,12 @@ public class SessaoTest {
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
+	
+	@Test
+	public void deveVenderTodosIngressosDisponiveis() throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(4);
+		
+		Assert.assertTrue(sessao.podeReservar(4));
+	}
 }
