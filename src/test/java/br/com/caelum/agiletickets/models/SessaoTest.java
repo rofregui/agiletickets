4abc2c02556b6d47ca0a644b8a9b11490bf560cf
Roberto <rofregui@yahoo.com.br>
2011-12-06 12:00:32 -0200

    Refatoração dos testes. Código comum de inicialização de Sessao foi separado.

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,41 +1,52 @@
 package br.com.caelum.agiletickets.models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SessaoTest {
 
+	private Sessao s;
+	
+	@Before
+	public void inicializa(){
+		this.s = new Sessao();
+	}
+	
 
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(2);
+        this.s.setTotalIngressos(2);
 
-        Assert.assertTrue(sessao.podeReservar(1));
+        Assert.assertTrue(this.s.podeReservar(1));
 	}
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(2);
+		this.s.setTotalIngressos(2);
 
-		Assert.assertFalse(sessao.podeReservar(3));
+		Assert.assertFalse(this.s.podeReservar(3));
 	}
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(5);
+		this.s.setTotalIngressos(5);
 
-		sessao.reserva(3);
-		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
+		this.s.reserva(3);
+		Assert.assertEquals(2, this.s.getIngressosDisponiveis().intValue());
 	}
 	
 	@Test
 	public void deveVenderTodosIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(4);
+		this.s.setTotalIngressos(4);
 		
-		Assert.assertTrue(sessao.podeReservar(4));
+		Assert.assertTrue(this.s.podeReservar(4));
+	}
+	
+	@Test
+	public void deveVenderTodosIngressosDisponiveisZero() throws Exception {
+		this.s.setTotalIngressos(0);
+		
+		Assert.assertTrue(this.s.podeReservar(0));
 	}
 }
